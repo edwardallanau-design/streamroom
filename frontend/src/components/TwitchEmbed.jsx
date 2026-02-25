8e1ba53c5418e95f39c53c27738c728d349ba748
Edward <edwardallan.au@gmail.com>
2026-02-25 16:20:29 +1100

    modified twitch embed implementation

--- frontend/src/components/TwitchEmbed.jsx
+++ frontend/src/components/TwitchEmbed.jsx
@@ -3,19 +3,22 @@ import '../styles/twitch.css'
 
 function TwitchEmbed() {
   const twitchChannel = import.meta.env.VITE_TWITCH_CHANNEL || 'your_channel_name'
+  const parent = window.location.hostname
 
   return (
     <div className="twitch-embed">
       <div className="twitch-container">
         <iframe
-          src={`https://player.twitch.tv/?channel=${twitchChannel}&parent=localhost`}
-          allow="autoplay"
+          src={`https://player.twitch.tv/?channel=${twitchChannel}&parent=${parent}`}
+          allow="autoplay; fullscreen"
+          allowFullScreen
           title="Twitch Stream"
         ></iframe>
       </div>
       <div className="twitch-chat">
         <iframe
-          src={`https://www.twitch.tv/embed/${twitchChannel}/chat?parent=localhost`}
+          src={`https://www.twitch.tv/embed/${twitchChannel}/chat?parent=${parent}`}
+          allowFullScreen
           title="Twitch Chat"
         ></iframe>
       </div>
